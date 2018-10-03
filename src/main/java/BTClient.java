5545e0418c6683dfd30137323bd0ffde83f38303
Juan Pablo Sanmiguel Mateus <jp.sanmiguel@uniandes.edu.co>
2018-10-03 07:41:26 -0500

    P2P sin automatizar

---
+++ src/main/java/BTClient.java
@@ -0,0 +1,66 @@
+import com.turn.ttorrent.client.Client;
+import com.turn.ttorrent.client.SharedTorrent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.security.NoSuchAlgorithmException;
+import java.util.Observable;
+import java.util.Observer;
+
+public class BTClient {
+
+    public BTClient() throws IOException, NoSuchAlgorithmException {
+        // First, instantiate the Client object.
+        Client client = new Client(
+                // This is the interface the client will listen on (you might need something
+                // else than localhost here).
+                InetAddress.getLocalHost(),
+                //InetAddress.getByAddress(new byte[]{0, 0, 0, 0}),
+
+                // Load the torrent from the torrent file and use the given
+                // output directory. Partials downloads are automatically recovered.
+                SharedTorrent.fromFile(
+				//Ruta torrent a descargar 
+                        new File("C:\\Users\\juanp\\Desktop\\pruebatr\\2.torrent"),
+				//Creo que ruta donde descarga (Parametro dice 'parentDirectory'
+                        new File("C:\\Users\\juanp\\Desktop\\pruebatr")));
+
+        client.addObserver(new Observer() {
+            @Override
+            public void update(Observable observable, Object data) {
+                Client client = (Client) observable;
+                float progress = client.getTorrent().getCompletion();
+                // Do something with progress.
+            }
+        });
+
+// You can optionally set download/upload rate limits
+// in kB/second. Setting a limit to 0.0 disables rate
+// limits.
+        client.setMaxDownloadRate(50.0);
+        client.setMaxUploadRate(50.0);
+
+// At this point, can you either call download() to download the torrent and
+// stop immediately after...
+        client.download();
+
+// Or call client.share(...) with a seed time in seconds:
+        client.share(3600);
+// Which would seed the torrent for an hour after the download is complete.
+
+// Downloading and seeding is done in background threads.
+// To wait for this process to finish, call:
+        client.waitForCompletion();
+
+// At any time you can call client.stop() to interrupt the download.
+        //client.stop();
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        BTClient client = new BTClient();
+
+
+
+    }
+}
---
+++ src/main/java/BTServer.java
@@ -0,0 +1,72 @@
+import com.turn.ttorrent.common.Torrent;
+import com.turn.ttorrent.tracker.TrackedTorrent;
+import com.turn.ttorrent.tracker.Tracker;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.security.NoSuchAlgorithmException;
+
+public class BTServer {
+
+    public BTServer() throws IOException, NoSuchAlgorithmException {
+
+    }
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+	//Codigo para crear torrent de un archivo
+        /*String sharedFile = "C:\\Users\\juanp\\Desktop\\Pruebatr\\2.docx";
+
+        try {
+            Tracker tracker = new Tracker( InetAddress.getLocalHost() );
+            tracker.start();
+            System.out.println("Tracker running.");
+
+            System.out.println( "create new .torrent metainfo file..." );
+            Torrent torrent = Torrent.create(new File(sharedFile), tracker.getAnnounceUrl().toURI(), "createdByGrupo4");
+
+            System.out.println("save .torrent to file...");
+
+            FileOutputStream fos = new FileOutputStream("C:\\torrent\\2.torrent");
+            torrent.save( fos );
+            fos.close();
+
+            tracker.stop();
+
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }*/
+
+        // First, instantiate a Tracker object with the port you want it to listen on.
+// The default tracker port recommended by the BitTorrent protocol is 6969.
+        Tracker tracker = new Tracker(new InetSocketAddress(6969));
+
+// Then, for each torrent you wish to announce on this tracker, simply created
+// a TrackedTorrent object and pass it to the tracker.announce() method:
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".torrent");
+            }
+        };
+
+		//Directorio con torrents que quiere anunciar
+        for (File f : new File("C:\\torrent").listFiles(filter)) {
+            tracker.announce(TrackedTorrent.load(f));
+        }
+
+        System.out.println(tracker.getAnnounceUrl().toString());
+        System.out.println(tracker.getTrackedTorrents().toString());
+
+// Once done, you just have to start the tracker's main operation loop:
+        tracker.start();
+
+// You can stop the tracker when you're done with:
+        //tracker.stop();
+
+    }
+}
